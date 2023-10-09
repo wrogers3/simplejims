b0f18f2aafcbfaebde8612d21720bae5df3d4d7a
Warren Rogers <warrenrogers127@gmail.com>
2023-10-10 09:45:27 +1100

    added basic shit

---
+++ api/backend/userprogram.ts
@@ -0,0 +1,18 @@
+// user functions
+import time
+
+function getFeedTiming(feedLocation: number, startTime: number, endTime: number, interval: number) : string {
+    // user programs how long and when the animals are fed, placeholder parameters because we dont know when theyre fed
+    const resultString = "we are feeding farm ${feedLocation}, within ${startTime} and ${endTime}";
+    return resultString;
+}
+
+function gateToggle (gateNum: number) : number {
+    // toggles a specific gate if its open we close it vice versa
+    return 0;
+}
+
+function togglePest (machineNum: number): number {
+    // toggles a pest control mechanism
+    return 0;
+}
